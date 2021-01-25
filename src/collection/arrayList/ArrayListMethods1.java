fc759a3c275b62d3e8e3f1e011493039666cabfb
AndreyWild <and1rei@mail.ru>
2021-01-25 16:57:00 +0300

    ArrayListMethods1

--- src/collection/arrayList/ArrayListMethods1.java
+++ src/collection/arrayList/ArrayListMethods1.java
@@ -1,4 +1,17 @@
 package collection.arrayList;
 
+import java.util.ArrayList;
+
 public class ArrayListMethods1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<String>(); // второй раз <String> можно не указывать
+        arrayList1.add("Андрей"); // метод .add() добавляет элемент с конец списка
+        arrayList1.add("Саша");
+        arrayList1.add("Мася");
+        arrayList1.add(1, "Миша"); // добавить Мина на 1 индекс
+        System.out.println(arrayList1); // [Андрей, Миша, Саша, Мася]
+
+        System.out.println(arrayList1.get(3)); // Мася
+
+    }
 }
