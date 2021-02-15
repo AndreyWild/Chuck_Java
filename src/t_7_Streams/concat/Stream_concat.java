5cebc67cd99991627fd9ee3023e7161595e570a7
AndreyWild <and1rei@mail.ru>
2021-02-15 21:46:32 +0300

    Streams

--- src/t_7_Streams/concat/Stream_concat.java
+++ src/t_7_Streams/concat/Stream_concat.java
@@ -8,7 +8,7 @@ public class Stream_concat {
         Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
         Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
         Stream<Integer> stream4 = Stream.concat(stream2, stream3);
-        stream4.forEach(System.out::println);
-        /** Нельзя после него вызвать другой Intermediate метод */
+        stream4.forEach(System.out::println); // 1 2 3 4 5 6 7 8 9 10
+        /** Метод объеденяет два стрима Нельзя после него вызвать другой Intermediate метод */
     }
 }
